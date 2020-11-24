6a1e2f6b2b3a2183d374486a63f06f311c82e55a
zhengquanliu <zhengquanliu@creditease.cn>
2020-11-24 09:41:08 +0800

    222. 完全二叉树的节点个数

---
+++ src/main/java/com.lzq.leecode/CountNodes.java
@@ -0,0 +1,29 @@
+package com.lzq.leecode;
+
+/**
+ * LeeCode
+ *
+ * 222. 完全二叉树的节点个数
+ */
+public class CountNodes {
+
+    private int count = 0;
+
+    public int countNodes(TreeNode root) {
+
+        if (root!=null){
+            count++;
+            countNodes(root.left);
+            countNodes(root.right);
+        }
+
+        return count;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
