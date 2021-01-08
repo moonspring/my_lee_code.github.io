ee6a5d0caaeba6e79bcc3cfa781c6139d6718806
zhengquanliu <zhengquanliu@creditease.cn>
2021-01-08 11:27:27 +0800

    176. Second Highest Salary

---
+++ src/main/java/com.lzq.leecode/SecondHighestSalary.java
@@ -0,0 +1,15 @@
+package com.lzq.leecode;
+
+/**
+ * LeeCode
+ *
+ * 176. Second Highest Salary
+ * @author lzq
+ */
+public class SecondHighestSalary {
+
+//    SELECT IFNULL((SELECT DISTINCT Salary
+//            FROM Employee
+//            ORDER BY Salary DESC
+//            LIMIT 1 OFFSET 1),NULL) AS SecondHighestSalary
+}
